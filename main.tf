3a8aa6190b87241a3161d0ff9fae7959710cdc35
petems <p.morsou+GIT@gmail.com>
2019-10-30 19:52:51 +0000

    Change to count 1

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "random_pet" "pet_quote_1" {
-  count = 1000
+  count = 1
 }
 
 resource "random_pet" "pet_quote_2" {
-  count = 1000
+  count = 1
 }
 
 resource "random_pet" "pet_quote_3" {
-  count = 1000
+  count = 1
 }
