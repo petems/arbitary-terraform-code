7f5060964a8c76cec8fd1f6afffffd7e698a7b72
Peter Souter <petems@users.noreply.github.com>
2020-03-17 15:18:22 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "random_pet" "pet_quote_1" {
-  count = 3
+  count = 5
 }
 
 resource "random_pet" "pet_quote_2" {
-  count = 3
+  count = 5
 }
 
 resource "random_pet" "pet_quote_3" {
-  count = 3
+  count = 5
 }
