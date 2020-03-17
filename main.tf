37454bc6f5c472787842131faff8e72674ef82d7
petems <p.morsou@gmail.com>
2020-03-17 15:23:20 +0000

    Change length

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "random_pet" "pet_quote_1" {
-  count = 5
+  length = 2
 }
 
 resource "random_pet" "pet_quote_2" {
-  count = 5
+  length = 2
 }
 
 resource "random_pet" "pet_quote_3" {
-  count = 5
+  length = 2
 }
